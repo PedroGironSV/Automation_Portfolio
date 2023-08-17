6debbf5231547f1f827fa5e46d8ed38e10248d23
Pedro Giron <pedro.giron1864@gmail.com>
2023-08-17 10:06:33 -0600

    Updated Api_Automation folder

--- API_Automation/Pytest-Python/pytest_runner.js
+++
@@ -1,9 +0,0 @@
-const shell = require('shelljs');
-
-function executeTest(){
-    reportPath = "./script/report/execution_report.html";
-    shell.exec('python -m pytest -v --html='+ reportPath +' ./script/JSON_Placeholder_testing.py');
-    shell.exec('start '+ reportPath);
-}
-
-executeTest();
